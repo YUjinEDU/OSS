abab9ba43d0367855be3969f5007731aca81619a
“YUjinEDU” <20191906@edu.hanbat.ac.kr>
2025-11-07 16:02:59 +0900

    내 수정사항 저장

---
+++ 20191906_Yujin_Park_week8.py
@@ -0,0 +1,26 @@
+
+import os
+import sys
+
+print("--------------------------------")
+print("I eat %d apples." % 3)
+print("I eat %s apples." % "five")
+print("I get %d, but I eat %s apples." % (15, "five"))
+
+
+
+a = "life is too short, you need python"
+print(a)
+print(a[0])
+print(a[0:4])
+print(a[0:2], a[5:7], a[12:17])
+
+b = "20191906YujinPark"
+id = b[0:6]
+name = b[6:14]
+
+print("id : ",id)
+print("name : ",name)
+
+c = "20191906" + "YujinPark"
+print("c : ",c)
